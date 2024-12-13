05d8bab599d7adf9037457973b08b672b03bc7fc
idMasjid <id@mosquee-massy.fr>
2024-12-13 23:40:52 +0100

    Add Next.js configuration files

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import NetatmoDashboard from '../components/netatmo-dashboard';
+
+export default function Home() {
+  return (
+    <main>
+      <NetatmoDashboard />
+    </main>
+  );
+}
